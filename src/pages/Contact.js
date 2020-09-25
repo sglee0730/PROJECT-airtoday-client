1499ff22d27e05f44a1533c758e4c588223ccb38
sglee0730 <stive2007@naver.com>
2020-09-25 10:12:54 +0900

    emailjs added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import '../ContactApp.css';
+import emailjs from 'emailjs-com'
 
 export default function Mappage() 
 {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_9xr4qmk', 'template_1ujmqfb', e.target, 'user_TzIpBnXNAkfY2RrmR79az')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      }
+
     return (
         <>
         <div class="contact-section">
             <h1>Contact Us</h1>
             <div class="contact-border"></div>
-            <form class="contact-form">
-                <input type="text" class="contact-form-text" placeholder="Your name"></input>
-                <input type="text" class="contact-form-text" placeholder="Your email"></input>
-                <textarea class="contact-form-text" placeholder="Your message"></textarea>
+            <form class="contact-form" onSubmit={sendEmail}>
+                <input type="text" class="contact-form-text" placeholder="Your name" name="user_name"></input>
+                <input type="text" class="contact-form-text" placeholder="Your email" name="user_email"></input>
+                <textarea class="contact-form-text" placeholder="Your message" name="message"></textarea>
                 <input type="submit" class="contact-form-btn" value="Send"></input>
             </form>
         </div>
