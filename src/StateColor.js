55c88d636302e22651e8a858479d8768637b1248
sglee0730 <stive2007@naver.com>
2020-10-29 13:37:34 +0900

    Fix stylesheet in Infowindow

--- src/StateColor.js
+++ src/StateColor.js
@@ -28,42 +28,39 @@ class getStateColor {
     }
   }
   noise = (data) => {
-    if (data < 0.7) {
+    if (data < 1.3) {
       return 'green'
     }
-    else if (data < 1.4) {
+    else if (data < 2.7) {
       return '#ffe259'
     }
-    else if (data < 2) {
-      return 'red'
-    }
     else {
-      return '#9B0000'
+      return 'red'
     }
   }
   temp = (data) => {
     if (data < 6) {
-      return '#0089FF'
+      return 'cornflowerblue'
     }
     else if (data < 17) {
-      return '#FF9D08'
+      return '#ffe259'
     }
     else if (data < 29) {
-      return '#00FF1A'
+      return 'green'
     }
     else {
-      return '#FFFB00'
+      return 'red'
     }
   }
   humi = (data) => {
     if (data < 31) {
-      return '#00DAFF'
+      return '#ffe259'
     }
     else if (data < 71) {
-      return '#00FF1A'
+      return 'green'
     }
     else {
-      return '#FF5500'
+      return '#ffe259'
     }
   }
 }
